e6c0d3ba3826659f508ede8417b60e87b991c9b2
Mikhail Glushkov <miggl@mail.ru>
2024-05-14 23:08:36 +0300

    create view and templates for client, message, subscribe

--- mailing/urls.py
+++ mailing/urls.py
@@ -2,15 +2,27 @@ from django.urls import path
 
 from mailing.apps import MailingConfig
 from mailing.views import ClientListView, ClientCreateView, MessageListView, SubscribeListView, SubscribeCreateView, \
-    MessageCreateView
+    MessageCreateView, SubscribeUpdateView, SubscribeDeleteView, SubscribeDetailView, ClientDetailView, \
+    ClientUpdateView, ClientDeleteView, MessageDetailView, MessageUpdateView, MessageDeleteView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
     path('', SubscribeListView.as_view(), name='home'),
     path('subscribe_create/', SubscribeCreateView.as_view(), name='subscribe_create'),
+    path('subscribe_detail/<int:pk>/', SubscribeDetailView.as_view(), name='subscribe_detail'),
+    path('subscribe_edit/<int:pk>/', SubscribeUpdateView.as_view(), name='subscribe_edit'),
+    path('subscribe_delete/<int:pk>/', SubscribeDeleteView.as_view(), name='subscribe_delete'),
+
     path('client_list/', ClientListView.as_view(), name='client_list'),
     path('client_create/', ClientCreateView.as_view(), name='client_create'),
+    path('client_detail/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
+    path('client_edit/<int:pk>/', ClientUpdateView.as_view(), name='client_edit'),
+    path('client_delete/<int:pk>/', ClientDeleteView.as_view(), name='client_delete'),
+
     path('message_list/', MessageListView.as_view(), name='message_list'),
     path('message_create/', MessageCreateView.as_view(), name='message_create'),
+    path('message_detail/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
+    path('message_edit/<int:pk>/', MessageUpdateView.as_view(), name='message_edit'),
+    path('message_delete/<int:pk>/', MessageDeleteView.as_view(), name='message_delete'),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from mailing.forms import ClientForm, SubscribeForm, MessageForm
 from mailing.models import Message, Client, SubscribeSettings
@@ -15,6 +15,21 @@ class SubscribeCreateView(CreateView):
     success_url = reverse_lazy('mailing:home')
 
 
+class SubscribeDetailView(DetailView):
+    model = SubscribeSettings
+
+
+class SubscribeUpdateView(UpdateView):
+    model = SubscribeSettings
+    form_class = SubscribeForm
+    success_url = reverse_lazy('mailing:home')
+
+
+class SubscribeDeleteView(DeleteView):
+    model = SubscribeSettings
+    success_url = reverse_lazy('mailing:home')
+
+
 class ClientListView(ListView):
     model = Client
 
@@ -25,6 +40,21 @@ class ClientCreateView(CreateView):
     success_url = reverse_lazy('mailing:client_list')
 
 
+class ClientDetailView(DetailView):
+    model = Client
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('mailing:client_list')
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('mailing:client_list')
+
+
 class MessageListView(ListView):
     model = Message
 
@@ -32,4 +62,19 @@ class MessageListView(ListView):
 class MessageCreateView(CreateView):
     model = Message
     form_class = MessageForm
+    success_url = reverse_lazy('mailing:message_list')
+
+
+class MessageDetailView(DetailView):
+    model = Message
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('mailing:message_list')
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
     success_url = reverse_lazy('mailing:message_list')
